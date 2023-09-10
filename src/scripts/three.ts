87374b7402e2688da4b53223d665692a379ce67d
joaquinns <joaquindevv96@gmail.com>
2023-09-09 22:29:01 -0400

    three js config

---
+++ src/scripts/three.ts
@@ -0,0 +1,91 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { RoomEnvironment } from 'three/addons/environments/RoomEnvironment.js'
+import Stats from 'three/addons/libs/stats.module.js'
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+
+const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
+renderer.setPixelRatio(window.devicePixelRatio)
+renderer.setSize(800, 600)
+
+function init3d(renderer: THREE.WebGLRenderer): void {
+  let mixer: THREE.AnimationMixer
+  const clock = new THREE.Clock()
+  const container = document.getElementById('container')
+
+  const stats = new Stats()
+
+  if (container !== null) {
+    container.appendChild(renderer.domElement)
+  }
+
+  const pmremGenerator = new THREE.PMREMGenerator(renderer)
+
+  const scene = new THREE.Scene()
+  // scene.background = new THREE.Color(0xbfe3dd)
+  scene.environment = pmremGenerator.fromScene(
+    new RoomEnvironment(renderer),
+    0.04
+  ).texture
+
+  const camera = new THREE.PerspectiveCamera(40, 800 / 600, 1, 100)
+  camera.position.set(5, 2, 8)
+
+  const controls = new OrbitControls(camera, renderer.domElement)
+  controls.target.set(0, 0.5, 0)
+  controls.update()
+  controls.enablePan = false
+  controls.enableDamping = true
+  controls.enableZoom = false
+
+  const dracoLoader = new DRACOLoader()
+  dracoLoader.setDecoderPath('jsm/libs/draco/gltf/')
+
+  const loader = new GLTFLoader()
+  loader.setDRACOLoader(dracoLoader)
+  loader.load(
+    '../src/models/glasses.glb',
+    function (gltf) {
+      const model = gltf.scene
+      model.position.set(1, 0, 0)
+      model.scale.set(0.3, 0.3, 0.3)
+      scene.add(model)
+
+      mixer = new THREE.AnimationMixer(model)
+
+      animate()
+    },
+    undefined,
+    function (e) {
+      console.error(e)
+    }
+  )
+
+  window.onresize = function () {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+
+    renderer.setSize(800, 600)
+  }
+
+  function animate(): void {
+    requestAnimationFrame(animate)
+
+    const delta = clock.getDelta()
+    scene.rotation.y += 0.005
+
+    mixer.update(delta)
+
+    controls.update()
+
+    stats.update()
+
+    renderer.render(scene, camera)
+  }
+}
+
+init3d(renderer)
+document.addEventListener('astro:after-swap', () => {
+  init3d(renderer)
+})
