b8d359652b34e054b8182c168947af24576b75a3
joaquinns <joaquindevv96@gmail.com>
2023-09-11 14:32:31 -0400

    static content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'server'
-});
+  integrations: [tailwind()]
+  // output: 'server'
+})
